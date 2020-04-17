98c18921a014f6986ad0811e3efdaeaa8c713528
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2020-04-17 22:44:29 +0300

    Create simple_iterations.py

---
+++ lab1/simple_iterations.py
@@ -0,0 +1,25 @@
+import math
+
+def func(x):
+    func_result = (math.sin(0.5 + x) + 0.5) / 2
+    return func_result
+
+def simple_iterations(a, b, accuracy):
+    x = (a + b) / 2
+    simple_iterations_accuracy = abs(b - a)
+
+    while simple_iterations_accuracy >= accuracy:
+        previous_result = x
+        x = func(x)
+        simple_iterations_accuracy = x - previous_result
+
+    return x
+
+a = float(input("Введите левую границу: "))
+b = float(input("Введите правую границу: "))
+accuracy = float(input("Введите точность: "))
+
+x, it_c = simple_iterations(a, b, accuracy)
+
+print ("Результат: " + str(x))
+print ("Он был получен за " + str(it_c) + " итераций")
